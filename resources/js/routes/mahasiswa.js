46d65aa23202fef07dfbb0c4a6d6137fb0794437
primagiant <kadekprimagiantmartadinata@gmail.com>
2021-12-07 23:17:44 +0800

    API Adalah Jalan Ninjaku

--- resources/js/routes/mahasiswa.js
+++ resources/js/routes/mahasiswa.js
@@ -1,7 +1,9 @@
 import Dashboard from '../mahasiswa/pages/Dashboard.vue'
-import Kegiatan from '../'
+import Kegiatan from '../mahasiswa/pages/Kegiatan.vue'
+import Portofolio from '../mahasiswa/pages/Portofolio.vue'
 
 export const MahasiswaRoutes = [
-    { name: 'dashboard', path:'mahasiswa/dashboard', component:Dashboard },
-    { name: 'kegiatan', path:'mahasiswa/kegiatan', component: Kegiatan},
+    { name: 'dashboard', path:'/mahasiswa/dashboard', component:Dashboard },
+    { name: 'kegiatan', path:'/mahasiswa/kegiatan', component: Kegiatan},
+    { name: 'portofolio', path:'/mahasiswa/portofolio', component: Portofolio},
 ];
